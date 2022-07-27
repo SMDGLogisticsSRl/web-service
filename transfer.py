a253399409f50f8c3c6e25b82d4a1e14e404e96c
SMDGLogisticsSRl <110128620+SMDGLogisticsSRl@users.noreply.github.com>
2022-07-27 21:44:10 +0200

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -4,8 +4,7 @@ from gsheetsdb import connect
 conn = connect()
 result = conn.execute("""
     SELECT
-        country
-      , SUM(cnt)
+         *
     FROM
         "https://docs.google.com/spreadsheets/d/1pCrJ9O3T6le6ptl-xUbbdGLhJ_J3SK6zgldJ5MJw6pA/edit?usp=sharing"
     GROUP BY
