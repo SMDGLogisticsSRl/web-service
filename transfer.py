4b82447953047c1ccfb52d7ca32d5b248bfa71c1
SMDGLogisticsSRl <110128620+SMDGLogisticsSRl@users.noreply.github.com>
2022-07-27 21:44:38 +0200

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -7,9 +7,7 @@ result = conn.execute("""
          *
     FROM
         "https://docs.google.com/spreadsheets/d/1pCrJ9O3T6le6ptl-xUbbdGLhJ_J3SK6zgldJ5MJw6pA/edit?usp=sharing"
-    GROUP BY
-        country
 """, headers=1)
 
 for row in result:
-    print(row)
+    st.write(row)
