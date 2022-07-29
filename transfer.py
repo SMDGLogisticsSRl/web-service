7cfcf34e0a3623c3caff55fa5eff62cc76196380
SMDGLogisticsSRl <110128620+SMDGLogisticsSRl@users.noreply.github.com>
2022-07-29 11:12:35 +0200

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -231,23 +231,6 @@ def custom_invoice():
                             #建立分单文件夹
 
                             wb = Workbook()
-                            ws = wb.active
-                            ws['A1'] = 42
-                            ws.append([1, 2, 3])
-                            ws['A2'] = 56
-                            wb.save("sample.xlsx")
-
-                            def get_binary_file_downloader_html(bin_file, file_label='File'):
-                                with open(file_path, 'rb') as f:
-                                    data = f.read()
-                                bin_str = base64.b64encode(data).decode()
-                                href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">点击下载 {file_label}</a>'
-                                return href
-                            file_path = 'sample.xlsx'
-                            file_label = '测试文件'
-                            st.markdown(get_binary_file_downloader_html(file_path, file_label),
-                                        unsafe_allow_html=True)
-
 
 
 
